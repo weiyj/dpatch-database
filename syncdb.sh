aee0d712f4b8efb5c745ad95396880bd46c3b82b
Wei Yongjun <weiyj.lk@gmail.com>
2013-05-05 12:56:52 +0800

    script: just fix a typo
    Signed-off-by: Wei Yongjun <weiyj.lk@gmail.com>

--- syncdb.sh
+++ syncdb.sh
@@ -2,6 +2,6 @@ cd /pub/scm/mygit/dpatch-database
 cp /var/lib/dpatch/database/sqlite.db .
 tar zcvf sqlite.db.tar.gz sqlite.db 
 unlink sqlite.db
-git commit -s -m "update to `date +'%Y%m%d'`"
+git commit -s . -m "update to `date +'%Y%m%d'`"
 git log -n 1
 
